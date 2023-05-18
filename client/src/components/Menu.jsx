9856a3f53c57a7689a2dc6e6f1c462d9ab17952e
Jiacheng-coder <2253541907@qq.com>
2023-05-18 17:46:52 +0800

    push

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,18 +1,13 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-const Home = () => {
+const Menu = ({cat}) => {
   // const [posts, setPosts] = useState([]);
-
-  const cat = useLocation().search
 
   // useEffect(() => {
   //   const fetchData = async () => {
   //     try {
-  //       const res = await axios.get(`/posts${cat}`);
+  //       const res = await axios.get(`/posts/?cat=${cat}`);
   //       setPosts(res.data);
   //     } catch (err) {
   //       console.log(err);
@@ -20,7 +15,7 @@ const Home = () => {
   //   };
   //   fetchData();
   // }, [cat]);
-
+  
   const posts = [
     {
       id: 1,
@@ -47,34 +42,18 @@ const Home = () => {
       img: "https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
   ];
-
-  const getText = (html) =>{
-    const doc = new DOMParser().parseFromString(html, "text/html")
-    return doc.body.textContent
-  }
-
-
   return (
-    <div className="home">
-      <div className="posts">
-        {posts.map((post) => (
-          <div className="post" key={post.id}>
-            <div className="img">
-              {/* <img src={`../upload/${post.img}`} alt="" /> */}
-              <img src={post.img} alt="" />
-            </div>
-            <div className="content">
-              <Link className="link" to={`/post/${post.id}`}>
-                <h1>{post.title}</h1>
-              </Link>
-              <p>{getText(post.desc)}</p>
-              <button>Read More</button>
-            </div>
-          </div>
-        ))}
-      </div>
+    <div className="menu">
+      <h1>Other posts you may like</h1>
+      {posts.map((post) => (
+        <div className="post" key={post.id}>
+          <img src={`../upload/${post?.img}`} alt="" />
+          <h2>{post.title}</h2>
+          <button>Read More</button>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Home;
+export default Menu;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,62 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App';
-import {
-  createBrowserRouter,
-  Outlet,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './pages/Home';
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Single from './pages/Single'
-import Write from './pages/Write'
-// components
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-
-const Layout = () => {
-  return (
-    <>
-      <Navbar/>
-      <Outlet/>
-      <Footer/>
-    </>
-  )
-}
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout/>,
-    children: [
-      {
-        path: '/',
-        element: <Home/>
-      },
-      {
-        path: '/post/:id',
-        element: <Single/>
-      },
-      {
-        path: '/write',
-        element: <Write/>
-      }
-    ]
-  },
-  {
-    path: "/test",
-    element: <div>This is test!</div>,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
